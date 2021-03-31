033f385f8b58fd4510586dae859de527b545b5b2
FacundoPonce <mrfacu125@gmail.com>
2021-03-31 10:48:09 -0300

    Modificado el offset de la camara respecto a planetas.

--- SolarSystem/Assets/Scripts/FocusPlanet.cs
+++ SolarSystem/Assets/Scripts/FocusPlanet.cs
@@ -7,26 +7,58 @@ public class FocusPlanet : MonoBehaviour
     public List<GameObject> planets;
     public Vector3 offset;
     private int index = 0;
+    private Vector3 auxOffset;
+    private void Start()
+    {
+        auxOffset = offset;
+    }
     void LateUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
             index = 0;
+            offset = auxOffset;
+        }
         if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
             index = 1;
+            offset = auxOffset;
+        }
         if (Input.GetKeyDown(KeyCode.Alpha3))
+        { 
             index = 2;
+            offset = auxOffset;
+        }
         if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
             index = 3;
+            offset = auxOffset;
+        }
         if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
             index = 4;
+            offset *= 2;
+        }
         if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
             index = 5;
+            offset *= 2;
+        }
         if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
             index = 6;
+            offset *= 2;
+        }
         if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
             index = 7;
+            offset *= 2;
+        }
         if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
             index = 8;
+            offset = auxOffset;
+        }
 
         if (planets[index] != null)
             this.transform.position = planets[index].transform.position + offset;
---
+++ SolarSystem/Assets/Scripts/MoveShip.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShip : MonoBehaviour
+{
+    [SerializeField] float shipSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0,0, (shipSpeed*0.5f) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, 0, (shipSpeed * 0.5f) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3((shipSpeed * 0.5f) * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3((shipSpeed * 0.5f) * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, 0, shipSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 0, -shipSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(-shipSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(shipSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
